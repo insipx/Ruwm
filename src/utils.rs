fc04ed1be89b63e300df302c55b88c57c902cb59
Andrew Plaza <aplaza@liquidthink.net>
2017-06-28 16:41:39 -0400

    type woes

--- src/utils.rs
+++ src/utils.rs
@@ -4,11 +4,14 @@ use xcb::ffi::xproto::xcb_atom_t as xcb_atom_t;
 struct Utils;
 
 impl Utils {
-  pub fn get_atoms<'a>(names: Vec<&str>, atoms: Vec<xcb_atom_t>, c: &xcb::Connection) {
-    let cookies: <std::vec::Vec<xcb::Cookie<'a, xcb::ffi::xcb_intern_atom_cookie_t>> as Trait>::new();
+  pub fn get_atoms(names: Vec<&str>, atoms: Vec<xcb_atom_t>, c: &xcb::Connection) {
+    type AtomicCookie<'a> = xcb::Cookie<'a, xcb::ffi::xproto::xcb_intern_atom_cookie_t>;
+    let cookies : std::vec::Vec<AtomicCookie>::new();
+
     for name in &names {
       cookies.push(xcb::intern_atom(c, false, name));
     }
+
     for (name, cookie) in names.iter().zip(cookies.iter()) {
       let reply = cookie.get_reply();
       match reply {
