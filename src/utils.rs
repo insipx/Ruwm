1d75125d4fe46c058f55611b07f945cabc667c3d
Andrew Plaza <aplaza@liquidthink.net>
2017-06-28 17:03:15 -0400

    works

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,15 @@
 extern crate xcb;
 use std;
 use xcb::ffi::xproto::xcb_atom_t as xcb_atom_t;
+use xcb::ffi::xproto::xcb_intern_atom_cookie_t as xcb_intern_atom_cookie_t;
 struct Utils;
 
 impl Utils {
-  pub fn get_atoms(names: Vec<&str>, atoms: Vec<xcb_atom_t>, c: &xcb::Connection) {
-    type AtomicCookie<'a> = xcb::Cookie<'a, xcb::ffi::xproto::xcb_intern_atom_cookie_t>;
-    let cookies : std::vec::Vec<AtomicCookie>::new();
+  pub fn get_atoms<'a>(names: Vec<&str>, mut atoms: Vec<xcb_atom_t>, c: &xcb::Connection) {
+
+    // type CookieAtom<'a> = xcb::Cookie<'a, xcb_intern_atom_cookie_t>;
+    let mut cookies : Vec<xcb::Cookie<'a, xcb_intern_atom_cookie_t>> = Vec::new();
+    // let mut cookies = Vec::<CookieAtom>::new();
 
     for name in &names {
       cookies.push(xcb::intern_atom(c, false, name));
@@ -16,7 +19,7 @@ impl Utils {
       let reply = cookie.get_reply();
       match reply {
         Ok(x) => atoms.push(x.atom()),
-        Err(e) => panic!("Error! {:?}", e),
+        Err(e) => panic!("Error! in get_atoms(), error: {:?}, name: , cookie:", e),
       }
     }
   }
