6ed1b40b13561672bec7094a8769ab25223fe8c5
Andrew Plaza <aplaza@liquidthink.net>
2017-07-24 22:23:00 -0400

    made progress in converting variables on the fly...but lots of cloning going on. There has to be a better way

--- src/config_parser/mod.rs
+++ src/config_parser/mod.rs
@@ -2,6 +2,13 @@ pub mod parser;
 use std::collections::HashMap;
 type Direction = String;
 
+/* 
+ * For some reason, the rustc compiler thinks this is all dead code
+ * probably since it's attached to the compiler, and the code that uses
+ * these structs is generated, the compiler does not pick up on this
+ * that is why this is all marked as allow(dead_code)
+ */
+
 #[derive(Debug)]
 #[allow(dead_code)]
 pub enum Config {
@@ -31,25 +38,42 @@ pub struct Variables {
 }
 
 impl Variables {
+  #[allow(dead_code)]
   pub fn new() -> Self {
 
     Variables {
       variables: HashMap::new(),
     }
   }
-
+  #[allow(dead_code)]
   pub fn set(&mut self, v: String, s: Vec<String>) -> Config {
     self.variables.insert(v.clone(), s.clone());
     Config::Set(v, s)
   }
-
+  #[allow(dead_code)]
   pub fn get(&self, k: &str) -> Vec<String> {
     let result = match self.variables.get(k) {
       Some(s) => s,
       None => panic!("Variable: {} not found!", k),
     };
 
-    return result;
+    return result.clone();
+  }
+
+  //if there should only be one symbol attached to a variable
+  // IE for workspaces
+  #[allow(dead_code)]
+  pub fn get_single(&self, k: &str) -> String {
+    let result = match self.variables.get(k) {
+      Some(s) => s,
+      None => panic!("Variable: {} not found!", k),
+    };
+
+    if result.len() > 1 {
+      panic!("More than one symbol attached to this variable!: {}", k);
+    }
+
+    String::from(result[0].clone())
   }
 }
 
